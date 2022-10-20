7803dbc8db791b2440e8f097a41c0852d7684aeb
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-20 19:44:37 +0100

    formatted code

--- challenges/2nd-level/2-censor-message.js
+++ challenges/2nd-level/2-censor-message.js
@@ -1,20 +1,32 @@
 function censorMessage(string, list) {
-    let rawWords = string.split(' ')
-    let resultArr = []
+  let rawWords = string.split(" ");
+  let resultArr = [];
 
-    for(let word of rawWords){
-        let lowerWord = word.toLowerCase()
-        if(list.includes(lowerWord)){
-            resultArr.push("*****")
-        }else{
-            resultArr.push(word)
-        }
+  for (let word of rawWords) {
+    let lowerWord = word.toLowerCase();
+    if (list.includes(lowerWord)) {
+      resultArr.push("*****");
+    } else {
+      resultArr.push(word);
     }
+  }
 
-    let message = resultArr.join(" ")
-    return message
+  let message = resultArr.join(" ");
+  return message;
 }
 
-console.log(censorMessage("don't mess with cats", ["mess"]), "should be:", "don't ***** with cats")
-console.log(censorMessage("Are you sure that this is safe", ["are", "is"]), "should be:", "***** you sure that this ***** safe")
-console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]), "should be:", "I CANNOT ***** *****")
+console.log(
+  censorMessage("don't mess with cats", ["mess"]),
+  "should be:",
+  "don't ***** with cats"
+);
+console.log(
+  censorMessage("Are you sure that this is safe", ["are", "is"]),
+  "should be:",
+  "***** you sure that this ***** safe"
+);
+console.log(
+  censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]),
+  "should be:",
+  "I CANNOT ***** *****"
+);
